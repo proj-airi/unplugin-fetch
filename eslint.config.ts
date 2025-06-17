d8332c52cbb4c7c61263946a9adc18e1acccb0df
Neko Ayaka <neko@ayaka.moe>
2025-06-17 14:47:18 +0800

    style: use @sumimakito integrated perfectionist/sort-imports
    Co-authored-by: Makito <5277268+sumimakito@users.noreply.github.com>

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,22 +1,31 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu(
-  {
-    unocss: false,
-    formatters: true,
-    markdown: false,
-    rules: {
-      'import/order': [
-        'error',
-        {
-          'groups': [
-            ['type'],
-            ['builtin', 'external'],
-            ['parent', 'sibling', 'index'],
-          ],
-          'newlines-between': 'always',
-        },
-      ],
-    },
+export default antfu({
+  unocss: false,
+  formatters: true,
+  markdown: false,
+  rules: {
+    'antfu/import-dedupe': 'error',
+    'import/order': 'off',
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        groups: [
+          'type-builtin',
+          'type-import',
+          'value-builtin',
+          'value-external',
+          'type-internal',
+          ['type-parent', 'type-sibling', 'type-index'],
+          'value-internal',
+          ['value-parent', 'value-sibling', 'value-index'],
+          ['wildcard-value-parent', 'wildcard-value-sibling', 'wildcard-value-index'],
+          'side-effect',
+          'style',
+        ],
+        newlinesBetween: 'always',
+      },
+    ],
+    'style/padding-line-between-statements': 'error',
   },
-)
+})
